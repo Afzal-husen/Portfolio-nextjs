1e8dc93acf4febc8a935d9a97d9ce22b93282955
Afzal Diwan <afzalhusen3@gmail.com>
2023-04-10 17:21:50 +0530

    added typed animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import DarkThemeLayout from "@/components/DarkThemeLayout";
 import Details from "@/components/Details";
 import styles from "../styles/Home.module.css";
+import Typed from "typed.js";
 import {
   GitHub,
   Twitter,
@@ -10,8 +11,33 @@ import {
   Instagram,
 } from "@mui/icons-material";
 import Link from "next/link";
+import { useEffect, useRef } from "react";
 
 const Home = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      strings: [
+        `<span>Hello </span> I Am <span>Afzal Diwan</span>
+        <br /> 
+        A <span>MERN</span> Stack Developer
+        <br /> 
+        A <span>Learning</span> Enthusiast
+        `,
+      ],
+      typeSpeed: 20,
+      backSpeed: 10,
+      loop: true,
+      cursorChar: "|",
+    };
+
+    const typed = new Typed(el.current, options);
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <>
       <DarkThemeLayout>
@@ -19,48 +45,38 @@ const Home = () => {
           <div className={styles.wrapper}>
             <div className={styles.heading}>
               <div>
-                <h4 className={styles.name}>
-                  <span>Hello </span> I Am <span>Afzal Diwan</span>
-                </h4>
-                <h4 className={styles.name}>
-                  A <span>MERN</span> Stack Developer
-                </h4>
-                <h4 className={styles.name}>
-                  A <span>Learning</span> Enthusiast
-                </h4>
+                <h4 ref={el} className={styles.name}></h4>
                 <div className={styles.btn}>
                   {/* social handles */}
                   <div className={styles.icons}>
-                    <a  href="https://github.com/Afzal-husen" target="_blank">
+                    <a href="https://github.com/Afzal-husen" target="_blank">
                       <GitHub className={styles.github} />
                     </a>
                     <a href="https://twitter.com/Afzal_Diwan_7" target="_blank">
                       <Twitter className={styles.twitter} />
                     </a>
-                    <a href="https://www.linkedin.com/in/afzal-diwan-7202b8239/" target="_blank">
+                    <a
+                      href="https://www.linkedin.com/in/afzal-diwan-7202b8239/"
+                      target="_blank">
                       <LinkedIn className={styles.linkdn} />
                     </a>
                     <a href="mailto:afzalhusen3@gmail.com">
                       <Google className={styles.google} />
                     </a>
-                    <a href="https://www.instagram.com/Afzal_Diwan_7/" target="_blank">
+                    <a
+                      href="https://www.instagram.com/Afzal_Diwan_7/"
+                      target="_blank">
                       <Instagram className={styles.insta} />
                     </a>
                   </div>
                   <button>
-                    <Link href="/Contact">
-                      Contact Me
-                    </Link>
+                    <Link href="/Contact">Contact Me</Link>
                   </button>
                 </div>
               </div>
             </div>
             <div className={styles.image}>
-              <Image
-              src={"/images/hello.svg"}
-              width={500}
-              height={500}
-              />
+              <Image src={"/images/hello.svg"} width={500} height={500} />
             </div>
           </div>
           <Details />
